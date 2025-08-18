64019d2299e28d99e59f9332b296687f880989e4
Débora <deboramarim77@hotmail.com>
2025-08-18 14:52:28 -0300

    .

---
+++ js/carousel.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    $('.blog-grid').slick({
+        dots: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '40px',
+                }
+            }
+        ]
+    });
+});
